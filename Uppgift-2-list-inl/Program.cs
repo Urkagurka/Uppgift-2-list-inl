bffbba36dc955c069d2d3576f78def97e84c66ab
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-02-05 20:45:03 +0100

    Add project files.

---
+++ Uppgift-2-list-inl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift_1_Listor_inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> talen = new List<int>();
+
+            while (true)
+            {
+                string tal = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(tal))
+                {
+                    break;
+                }
+                talen.Add(int.Parse(tal));
+            }
+
+            int störst = Max(talen);
+            Console.WriteLine(störst);
+        }
+        static int Max(List<int> talen2)
+        {
+            talen2.Sort();
+            return talen2[talen2.Count - 1];
+            
+
+        }
+    }
+}
